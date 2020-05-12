0b68f72bf7e09ea0d57fdd2fa3b704df2fcf78f2
Melissa <malvarezc_1@uqvirtual.edu.co>
2020-05-12 16:49:42 -0500

    get y set red

--- src/co/sis/crimewil/persistencia/Red.java
+++ src/co/sis/crimewil/persistencia/Red.java
@@ -12,7 +12,45 @@ public class Red {
 		this.direccionRed = direccionRed;
 		this.mascaraRed = mascaraRed;
 	}
-	
-	
+
+	public String getDireccionRed() {
+		return direccionRed;
+	}
+
+	public void setDireccionRed(String direccionRed) {
+		this.direccionRed = direccionRed;
+	}
+
+	public String getBroadcast() {
+		return broadcast;
+	}
+
+	public void setBroadcast(String broadcast) {
+		this.broadcast = broadcast;
+	}
+
+	public int getCantHost() {
+		return cantHost;
+	}
+
+	public void setCantHost(int cantHost) {
+		this.cantHost = cantHost;
+	}
+
+	public String[] getRangoIP() {
+		return rangoIP;
+	}
+
+	public void setRangoIP(String[] rangoIP) {
+		this.rangoIP = rangoIP;
+	}
+
+	public int getMascaraRed() {
+		return mascaraRed;
+	}
+
+	public void setMascaraRed(int mascaraRed) {
+		this.mascaraRed = mascaraRed;
+	}
 	
 }
