d1c7ef9e5296d305bbf6c321cc330eb0deb3b3a0
Melissa <malvarezc_1@uqvirtual.edu.co>
2020-05-14 14:06:15 -0500

    Pruebas unitarias hechas
    (una prueba para cada metodo de la calculadora)

--- src/co/sis/crimewil/persistencia/Calculadora.java
+++ src/co/sis/crimewil/persistencia/Calculadora.java
@@ -24,14 +24,38 @@ public class Calculadora {
 		String mascaraRedDecimal = co.sis.crimewil.util.Util.obtenerDireccionDecimal(direccionRed);
 		int[] direccionBroadcast = co.sis.crimewil.util.Util.calcularDireccionBroadcast(direccionRed, mascaraRed);
 		int cantDireccionesIP = co.sis.crimewil.util.Util.obtenerCantDireccionesIP(mascaraRed);		
-		int cantBitsRed = co.sis.crimewil.util.Util.obtenerCantDatosArray(direccionRed, 1);
-		int cantBitsHosts = co.sis.crimewil.util.Util.obtenerCantDatosArray(direccionRed, 0);
+		int cantBitsRed = co.sis.crimewil.util.Util.obtenerCantDatosArray(mascaraRed, 1);
+		int cantBitsHosts = co.sis.crimewil.util.Util.obtenerCantDatosArray(mascaraRed, 0);
 		ArrayList<int[]> rangoIP = co.sis.crimewil.util.Util.obtenerRangoIPs(direccionRed, direccionBroadcast);
 		
 		Red red = new Red(direccionRed, mascaraRed, direccionBroadcast, cantDireccionesIP, rangoIP, cantBitsRed, cantBitsHosts, mascaraRedDecimal);
 		
 		return red;
 		
+	}
+	
+	public int[] convertirDEC_BIN (int dec) {
+		return co.sis.crimewil.util.Util.convertirDecimalABinario(dec);
+	}
+	
+	public String convertirDEC_HEX (int dec) {
+		return co.sis.crimewil.util.Util.convertirDecimalAHexa(dec);
+	}
+	
+	public int convertirBIN_DEC (int bin[]) {
+		return co.sis.crimewil.util.Util.convertirBinarioADecimal(bin);
+	}
+	
+	public String convertirBIN_HEX (int bin[]) {
+		return co.sis.crimewil.util.Util.convertirBinarioAHexa(bin);
+	}
+	
+	public int convertirHEX_DEC (String hex) {
+		return co.sis.crimewil.util.Util.convertirHexaADecimal(hex);
+	}
+	
+	public int[] convertirHEX_BIN (String hex) {
+		return co.sis.crimewil.util.Util.convertirHexaABinario(hex);
 	}
 
 }
