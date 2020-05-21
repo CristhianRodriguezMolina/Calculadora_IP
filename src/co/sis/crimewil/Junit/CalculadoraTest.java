38de2af4b267525524d4c1d1c9f6be4db0818a55
Melissa <malvarezc_1@uqvirtual.edu.co>
2020-05-20 22:29:27 -0500

    pruebas JUNIT

--- src/co/sis/crimewil/Junit/CalculadoraTest.java
+++ src/co/sis/crimewil/Junit/CalculadoraTest.java
@@ -13,20 +13,18 @@ import co.sis.crimewil.persistencia.Red;
 
 class CalculadoraTest {
 	
-	private Calculadora calculadora = new Calculadora();
-	
 	@Test
-	public void testCalcularDatosHost() {
+	public void testCalcularDatosHost_1() {
 		int direccionHost[] = {1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,1,0,0,0}; //195.50.100.8
-		int mascaraRed[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0}; //24 bits
-		Red resultado = calculadora.calcularDatosHost(direccionHost, mascaraRed);
+		int mascaraRed[] 	= {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0}; //24 bits
+		Red resultado = Calculadora.calcularDatosHost(direccionHost, mascaraRed);
 		
-		int[] direccionRedEsperado = {1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0}; //195.50.100.0
-		int[] mascaraRedEsperado = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0}; //24 bits
-		int[] direccionBroadcastEsperado = {1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,0,0,1,1,1,1,1,1,1,1}; //195.50.100.255
+		int[] direccionRedEsperado 			= {1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0}; //195.50.100.0
+		int[] mascaraRedEsperado 			= {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0}; //24 bits
+		int[] direccionBroadcastEsperado 	= {1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,0,0,1,1,1,1,1,1,1,1}; //195.50.100.255
 		int cantDireccionesIPEsperado = 254;
-		int[] rangoInicioEsperado = {1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,1}; //195.50.100.1
-		int[] rangoFinalEsperado = {1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,0,0,1,1,1,1,1,1,1,0}; //195.50.100.254
+		int[] rangoInicioEsperado 			= {1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,1}; //195.50.100.1
+		int[] rangoFinalEsperado 			= {1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,0,0,1,1,1,1,1,1,1,0}; //195.50.100.254
 		
 		assertTrue(Arrays.equals(direccionRedEsperado, resultado.getDireccionRed()));
 		assertTrue(Arrays.equals(mascaraRedEsperado, resultado.getMascaraRed()));
@@ -37,10 +35,31 @@ class CalculadoraTest {
 	}
 	
 	@Test
-	public void testCalcularDatosRed() {
+	public void testCalcularDatosHost_2() {
+		int direccionHost[] = {1,1,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0}; //192.168.0.10
+		int mascaraRed[] 	= {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0}; //21 bits
+		Red resultado = Calculadora.calcularDatosHost(direccionHost, mascaraRed);
+		
+		int[] direccionRedEsperado 			= {1,1,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //192.168.0.0
+		int[] mascaraRedEsperado 			= {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0}; //21 bits
+		int[] direccionBroadcastEsperado 	= {1,1,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1}; //192.168.7.255
+		int cantDireccionesIPEsperado = 2048;
+		int[] rangoInicioEsperado 			= {1,1,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}; //192.168.0.1
+		int[] rangoFinalEsperado 			= {1,1,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0}; //192.168.7.254
+		
+		assertTrue(Arrays.equals(direccionRedEsperado, resultado.getDireccionRed()));
+		assertTrue(Arrays.equals(mascaraRedEsperado, resultado.getMascaraRed()));
+		assertTrue(Arrays.equals(direccionBroadcastEsperado, resultado.getDireccionBroadcast()));
+		assertTrue(cantDireccionesIPEsperado == resultado.getCantHost());
+		assertTrue(Arrays.equals(rangoInicioEsperado, resultado.getRangoIP().get(0)));
+		assertTrue(Arrays.equals(rangoFinalEsperado, resultado.getRangoIP().get(resultado.getRangoIP().size()-1)));
+	}
+	
+	@Test
+	public void testCalcularDatosRed_1() {
 		int direccionRed[] = {1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0}; //195.50.100.0
 		int mascaraRed[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0}; //24 bits
-		Red resultado = calculadora.calcularDatosRed(direccionRed, mascaraRed);
+		Red resultado = Calculadora.calcularDatosRed(direccionRed, mascaraRed);
 		
 		int[] direccionRedEsperado = {1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0}; //195.50.100.0
 		int[] mascaraRedEsperado = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0}; //24 bits
@@ -50,7 +69,7 @@ class CalculadoraTest {
 		int[] rangoFinalEsperado = {1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,0,0,1,1,1,1,1,1,1,0}; //195.50.100.254
 		int cantBitsRed = 24;
 		int cantBitsHosts = 8;
-		String mascaraRedDecimal = "195.50.100.0";
+		String mascaraRedDecimal = "255.255.255.0";
 		
 		assertTrue(Arrays.equals(direccionRedEsperado, resultado.getDireccionRed()));
 		assertTrue(Arrays.equals(mascaraRedEsperado, resultado.getMascaraRed()));
@@ -64,9 +83,111 @@ class CalculadoraTest {
 	}
 	
 	@Test
-	public void tesConvertirDEC_BIN() {
+	public void testCalcularDatosRed_2() {
+		int direccionRed[] = {1,1,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //192.168.0.0
+		int mascaraRed[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0}; //21 bits
+		Red resultado = Calculadora.calcularDatosRed(direccionRed, mascaraRed);
+		
+		int[] direccionRedEsperado = {1,1,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //192.168.0.0
+		int[] mascaraRedEsperado = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0}; //21 bits
+		int[] direccionBroadcastEsperado = {1,1,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1}; //192.168.7.255
+		int cantDireccionesIPEsperado = 2046;
+		int[] rangoInicioEsperado = {1,1,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //192.168.0.0
+		int[] rangoFinalEsperado = {1,1,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0}; //192.168.7.254
+		int cantBitsRed = 21;
+		int cantBitsHosts = 11;
+		String mascaraRedDecimal = "255.255.248.0";
+		
+		assertTrue(Arrays.equals(direccionRedEsperado, resultado.getDireccionRed()));
+		assertTrue(Arrays.equals(mascaraRedEsperado, resultado.getMascaraRed()));
+		assertTrue(Arrays.equals(direccionBroadcastEsperado, resultado.getDireccionBroadcast()));
+		assertTrue(cantDireccionesIPEsperado == resultado.getCantHost());
+		assertTrue(Arrays.equals(rangoInicioEsperado, resultado.getRangoIP().get(0)));
+		assertTrue(Arrays.equals(rangoFinalEsperado, resultado.getRangoIP().get(resultado.getRangoIP().size()-1)));
+		assertEquals(cantBitsRed, resultado.getCantBitsRed());
+		assertEquals(cantBitsHosts, resultado.getCantBitsHosts());
+		assertEquals(mascaraRedDecimal, resultado.getMascaraRedDecimal());
+	}
+	
+	@Test
+	public void tesConvertirDEC_BIN_1() {
 		int dec = 10;
-		int bin[] = calculadora.convertirDEC_BIN(dec);
+		int bin[] = Calculadora.convertirDEC_BIN(dec);
+		
+		int binEsperado[] = {1,0,1,0};
+		
+		assertTrue(Arrays.equals(binEsperado, bin));
+	}
+	@Test
+	public void tesConvertirDEC_BIN_2() {
+		int dec = 10;
+		int bin[] = Calculadora.convertirDEC_BIN(dec);
+		
+		int binEsperado[] = {1,0,0,0};
+		
+		assertTrue(Arrays.equals(binEsperado, bin));
+	}
+	
+	@Test
+	public void tesConvertirDEC_HEX_1() {
+		int dec = 10;
+		String hex = Calculadora.convertirDEC_HEX(dec);
+		assertEquals("A", hex);
+	}
+	
+	@Test
+	public void tesConvertirDEC_HEX_2() {
+		int dec = 10;
+		String hex = Calculadora.convertirDEC_HEX(dec);
+		assertEquals("B", hex);
+	}
+	
+	@Test
+	public void tesConvertirBIN_DEC_1() {
+		int[] bin = {0,0,0,0,1,0,1,0};
+		int dec = Calculadora.convertirBIN_DEC(bin);
+		assertEquals(10, dec);
+	}
+	
+	@Test
+	public void tesConvertirBIN_DEC_2() {
+		int[] bin = {0,0,0,0,1,0,1,0};
+		int dec = Calculadora.convertirBIN_DEC(bin);
+		assertEquals(8, dec);
+	}
+	
+	@Test
+	public void tesConvertirBIN_HEX_1() {
+		int[] bin = {0,0,0,0,1,0,1,0};
+		String hex = Calculadora.convertirBIN_HEX(bin);
+		assertEquals("A", hex);
+	}
+	
+	@Test
+	public void tesConvertirBIN_HEX_2() {
+		int[] bin = {0,0,0,0,1,0,1,0};
+		String hex = Calculadora.convertirBIN_HEX(bin);
+		assertEquals("D", hex);
+	}
+	
+	@Test
+	public void tesConvertirHEX_DEC_1() {
+		String hex = "A";
+		int dec = Calculadora.convertirHEX_DEC(hex);
+		assertEquals(10, dec);
+	}
+	
+	@Test
+	public void tesConvertirHEX_DEC_2() {
+		String hex = "A";
+		int dec = Calculadora.convertirHEX_DEC(hex);
+		assertEquals(15, dec);
+	}
+	
+	@Test
+	public void tesConvertirHEX_BIN_1() {
+		String hex = "A";
+		int bin[] = Calculadora.convertirHEX_BIN(hex);
 		
 		int binEsperado[] = {1,0,1,0};
 		
@@ -74,39 +195,11 @@ class CalculadoraTest {
 	}
 	
 	@Test
-	public void tesConvertirDEC_HEX() {
-		int dec = 10;
-		String hex = calculadora.convertirDEC_HEX(dec);
-		assertEquals("a", hex);
-	}
-	
-	@Test
-	public void tesConvertirBIN_DEC() {
-		int[] bin = {0,0,0,0,1,0,1,0};
-		int dec = calculadora.convertirBIN_DEC(bin);
-		assertEquals(10, dec);
-	}
-	
-	@Test
-	public void tesConvertirBIN_HEX() {
-		int[] bin = {0,0,0,0,1,0,1,0};
-		String hex = calculadora.convertirBIN_HEX(bin);
-		assertEquals("a", hex);
-	}
-	
-	@Test
-	public void tesConvertirHEX_DEC() {
-		String hex = "a";
-		int dec = calculadora.convertirHEX_DEC(hex);
-		assertEquals(10, dec);
-	}
-	
-	@Test
-	public void tesConvertirHEX_BIN() {
-		String hex = "a";
-		int bin[] = calculadora.convertirHEX_BIN(hex);
+	public void tesConvertirHEX_BIN_2() {
+		String hex = "A";
+		int bin[] = Calculadora.convertirHEX_BIN(hex);
 		
-		int binEsperado[] = {1,0,1,0};
+		int binEsperado[] = {1,0,0,1};
 		
 		assertTrue(Arrays.equals(binEsperado, bin));
 	}
